5f8b1aeb041f8bcfa070af03f3cdc9b11c463cbb
madi-orazbek <madiorazbek546@gmail.com>
2025-08-08 02:19:11 +0500

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -14,7 +14,7 @@ export default function Register() {
     e.preventDefault();
     try {
       setError('');
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://uniwork-1.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
